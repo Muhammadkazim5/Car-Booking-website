10bd31913ccd6e62360a744b11b6cb0c06aed8b0
Muhammad Kazim <133555661+Muhammadkazim5@users.noreply.github.com>
2026-05-27 10:52:30 +0500

    Add additional paragraph to AboutUs component

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -12,6 +12,8 @@
           voluptas adipisci nobis sequi!</p>
         <p class="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum laboriosam sed nobis
           reiciendis nesciunt doloremque mollitia magni sint molestiae quam!</p>
+        <p class="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum laboriosam sed nobis
+          reiciendis nesciunt doloremque mollitia magni sint molestiae quam!</p>
         <CustomButton text="Search Vehicle" />
       </div>
     </div>
