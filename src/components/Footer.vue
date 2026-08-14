a59d832b28013cad458e866d3a1a2cf059198356
Muhammad Kazim <133555661+Muhammadkazim5@users.noreply.github.com>
2026-08-14 17:39:38 +0500

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -34,7 +34,7 @@
 
       <div>
         <p class="text-gray-200 text-md text-center mt-16">
-          Copyright ©2025 Made with love by Code ❤️ Duchess
+          Copyright ©2026 Made with love by Code ❤️ Duchess
         </p>
       </div>
     </div>
